9963c2ed46513cd633ade4f740191bacff78ecb3
Christoph Stephan <chm.stephan@outlook.com>
2015-09-26 19:41:57 +0200

    Anonymous type sources

--- LatexScriptWrapper/ExtensionMethods/AnonymousTypeHelper.cs
+++ LatexScriptWrapper/ExtensionMethods/AnonymousTypeHelper.cs
@@ -4,12 +4,19 @@ using System.Dynamic;
 using System.Linq;
 using System.Reflection;
 using System.Runtime.CompilerServices;
-using System.Text;
 
 namespace ExtensionMethods
 {
     public static class AnonymousTypeHelper
     {
+        /// <summary>
+        /// source: http://stackoverflow.com/questions/2630370/c-sharp-dynamic-cannot-access-properties-from-anonymous-types-declared-in-anot
+        /// with little modifications
+        ///  - recursive
+        ///  - check for anonymous type
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns></returns>
         public static object ToExpandoObjectIfNecessary(this object obj)
         {
             if (obj == null)
@@ -32,13 +39,33 @@ namespace ExtensionMethods
             return expandoObj as ExpandoObject;
         }
 
-        public static Boolean IsAnonymousType(this Type type)
-        {
-            Boolean hasCompilerGeneratedAttribute = type.GetCustomAttributes(typeof(CompilerGeneratedAttribute), false).Count() > 0;
-            Boolean nameContainsAnonymousType = type.FullName.Contains("AnonymousType");
-            Boolean isAnonymousType = hasCompilerGeneratedAttribute && nameContainsAnonymousType;
 
-            return isAnonymousType;
+        /// <summary>
+        /// Determine through reflection if this type is an compiler generated anonymous type
+        /// 
+        /// There is no C# language construct which allows you to say
+        /// "Is this an anonymous type". You can use a simple heuristic
+        /// to approximate if a type is an anonymous type, but it's possible
+        /// to get tricked by people hand coding IL, or using a language where
+        /// such characters as > and < are valid in identifiers.
+        /// 
+        /// source: http://stackoverflow.com/questions/2483023/how-to-test-if-a-type-is-anonymous
+        /// </summary>
+        /// <param name="type">the type to check</param>
+        /// <returns>true if the passed type is a compiler generated anonymous type</returns>
+        /// <remarks>This code is copied from http://www.liensberger.it/web/blog/?p=191 </remarks>
+        public static bool IsAnonymousType(this Type type)
+        {
+            if (type == null)
+                throw new ArgumentNullException("type");
+
+            // TODO: The only way to detect anonymous types right now.
+            return Attribute.IsDefined(type, typeof(CompilerGeneratedAttribute), false)
+                   && type.IsGenericType
+                   && type.Name.Contains("AnonymousType")
+                   && (type.Name.StartsWith("<>", StringComparison.OrdinalIgnoreCase) || type.Name.StartsWith("VB$", StringComparison.OrdinalIgnoreCase))
+                   && (type.Attributes & TypeAttributes.NotPublic) == TypeAttributes.NotPublic;
         }
+
     }
 }

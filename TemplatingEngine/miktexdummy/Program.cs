632f88ce102f45fcbf7f3e1e6c0a43fe60a042d1
STC <stc@spangler-automation.de>
2018-03-23 15:03:37 +0100

    recycle CSScriptCompilerTest into new project

---
+++ TemplatingEngine/miktexdummy/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.IO;
+using System.Text;
+
+namespace miktexdummy {
+
+    public class Program {
+
+        private static void Main(string[] args) {
+            File.WriteAllText(Path.ChangeExtension(args[0], ".pdf"), @"Ich bin ein PDF :D
+" + File.ReadAllText(args[0]), Encoding.UTF8);
+            File.WriteAllText(Path.ChangeExtension(args[0], ".log"), "Ich bin ein log :D", Encoding.UTF8);
+        }
+    }
+}

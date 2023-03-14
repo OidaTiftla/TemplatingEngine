2ad7e1e9b6005b2d5b8938d671a1a1aa53a74f36
OidaTiftla <oidatiftla@oidatiftla.de>
2023-03-14 18:29:40 +0100

    Introduce ResolveRelativeTo

--- LatexScriptWrapper/ExtensionMethods/IOExtension.cs
+++ LatexScriptWrapper/ExtensionMethods/IOExtension.cs
@@ -2,6 +2,38 @@
 
 namespace ExtensionMethods {
     public static partial class IOExtension {
+        #region resolve relative
+
+        public static string ResolveRelativeTo(this string relativePath, FileInfo file) {
+            return relativePath.ResolveRelativeTo(file.Directory ?? new DirectoryInfo(""));
+        }
+
+        public static string ResolveRelativeTo(this string relativePath, DirectoryInfo dir) {
+            // source: http://stackoverflow.com/questions/703281/getting-path-relative-to-the-current-working-directory
+            // with my adjustments
+
+            // Folders must end in a slash
+            var folder = dir.FullName;
+            if (!folder.EndsWith(Path.DirectorySeparatorChar.ToString(), StringComparison.InvariantCulture)) {
+                folder += Path.DirectorySeparatorChar;
+            }
+            var baseUri = new Uri(folder);
+
+            var resolvedUri = new Uri(baseUri, relativePath);
+            var rel = Uri.UnescapeDataString(resolvedUri.AbsolutePath);
+            if (string.IsNullOrWhiteSpace(rel)) {
+                rel = "./";
+            }
+
+            return rel.Replace('/', Path.DirectorySeparatorChar);
+        }
+
+        public static string ResolveRelativeToExecutingAssembly(this string relativPath) {
+            return Path.Combine(AppContext.BaseDirectory, relativPath);
+        }
+
+        #endregion resolve relative
+
         #region file info
         public static string NameWithoutExtension(this FileInfo file) {
             return Path.GetFileNameWithoutExtension(file.Name);
